73679190d78bbdeda1d09149099b135ec3d2fcd0
Zaeem Ahmed <zaeemahmed@gmail.com>
2026-08-19 20:08:44 +0500

    OK

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5174' || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'http://localhost:5174' || 'http://localhost:5173' || 'https://ecommerce-platform.vercel.app',
   credentials: true,
 }));
 app.use(express.json());
